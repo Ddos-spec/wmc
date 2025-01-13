fb317ae6db621bd17809dbc7d0403a6c3cdb0e59
Ddos-spec <setgraph69@gmail.com>
2025-01-13 20:14:24 +0700

    perbaikan bug

--- home.php
+++ home.php
@@ -165,7 +165,7 @@ body {
 <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
         <a class="navbar-brand" href="#">
-            <img src="gambar/LOGO-WIMCYCLE-e1670387342889.png" alt="Logo">
+<img src="gambar/logo.png" alt="Logo">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
